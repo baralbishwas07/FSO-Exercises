3d12444f2b9f15fcd7e56b8a609f880fa40179a2
Bishwas Baral <bishwasbaral50@gmail.com>
2025-01-20 18:41:50 +0545

    solution to exercise 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -20,34 +20,36 @@ const Content = ({parts}) => {
   )
 }
 
-const Total = ({total}) => {
+const Total = ({parts}) => {
   return (
-    <p>Number of exercises {total}</p>
+    <p>Number of exercises {parts[0].exercises+parts[1].exercises+parts[2].exercises}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts}/>
-      <Total total={parts[0].exercises+parts[1].exercises+parts[2].exercises}/>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
