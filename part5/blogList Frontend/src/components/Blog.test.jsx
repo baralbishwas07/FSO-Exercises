3767423daa05d587c57caac25244f619925d4eec
Bishwas Baral <bishwasbaral50@gmail.com>
2025-04-25 22:19:04 +0545

    solution to exercise 5.14

--- part5/blogList Frontend/src/components/Blog.test.jsx
+++ part5/blogList Frontend/src/components/Blog.test.jsx
@@ -1,33 +1,62 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders title and author but not url and number of likes', () => {
-    const blog = {
-        id: '1',
-        title: 'Blog rendering',
-        author: 'Test user',
-        url: 'http://blogrendering.com',
-        likes: 19,
-        user: {
-            id: '123',
+describe('<Blog />', () => {
+
+    let container
+    let blog 
+    let loggedUser
+    let mockUpdateBlog
+    let mockDeleteBlog
+
+    beforeEach(() => {
+        blog = {
+            id: '1',
+            title: 'Blog rendering',
+            author: 'Test user',
+            url: 'http://blogrendering.com',
+            likes: 19,
+            user: {
+                id: '123',
+                username: 'testuser',
+                name: 'TestUser'
+            }
+        }
+
+        loggedUser = {
             username: 'testuser',
             name: 'TestUser'
         }
-    }
 
-    const loggedUser = {
-        username: 'testuser',
-        name: 'TestUser'
-    }
+        mockUpdateBlog = vi.fn()
+        mockDeleteBlog = vi.fn()
 
-    const mockUpdateBlog = vi.fn()
-    const mockDeleteBlog = vi.fn()
+        container = render(<Blog blog={blog} updateBlog={mockUpdateBlog} deleteBlog={mockDeleteBlog} loggedUser={loggedUser} />).container
+    })
 
-    const { container } = render(<Blog blog={blog} updateBlog={mockUpdateBlog} deleteBlog={mockDeleteBlog} loggedUser={loggedUser} />)
+    test('renders title and author but not url and number of likes', async () => {
 
-    const summary = container.querySelector('.summary-blog')
-    expect(summary).toHaveTextContent('Blog rendering')
-    expect(summary).toHaveTextContent('Test user')
-    expect(summary).not.toHaveTextContent('http://blogrendering.com')
-    expect(summary).not.toHaveTextContent('19')
+        const summary = container.querySelector('.summary-blog')
+        expect(summary).toHaveTextContent('Blog rendering')
+        expect(summary).toHaveTextContent('Test user')
+        expect(summary).not.toHaveTextContent('http://blogrendering.com')
+        expect(summary).not.toHaveTextContent('19')
+    })
+
+    test('clicking a button shows the blog url and likes', async () => {
+        const user = userEvent.setup()
+        const viewButton = screen.getByText('view')
+        await user.click(viewButton)
+
+        // const details = container.querySelector('.detailed-blog')
+        // expect(details).toHaveTextContent('http://blogrendering.com')
+        // expect(details).toHaveTextContent('19')
+
+        const url = screen.getByText('http://blogrendering.com')
+        const likes = screen.getByText('19')
+        expect(url).toBeDefined()
+        expect(likes).toBeDefined()
+    })
+
 })
